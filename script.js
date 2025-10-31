c536b7d759662e4a0b6ee6d1d33829a0a297bf14
nguyenthuhuyen <huyen@example.com>
2025-10-31 13:25:57 +0700

    them javascript

--- script.js
+++ script.js
@@ -4,16 +4,23 @@ function upDate(previewPic) {
   console.log("Image source:", previewPic.src);
 
   const imageDiv = document.getElementById("image");
-
   imageDiv.innerHTML = previewPic.alt;
-
   imageDiv.style.backgroundImage = `url('${previewPic.src}')`;
 }
 
 function undo() {
   const imageDiv = document.getElementById("image");
-
   imageDiv.style.backgroundImage = "url('')";
-
   imageDiv.innerHTML = "Hover over an image below to display here.";
 }
+
+function addTabFocus() {
+  console.log("addTabFocus triggered!");
+
+  const images = document.querySelectorAll(".preview");
+  for (let i = 0; i < images.length; i++) {
+    images[i].setAttribute("tabindex", "0");
+  }
+}
+
+window.onload = addTabFocus;
